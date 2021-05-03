1d4480ec6a9bcc58060420f47cec1d56d180a516
troll530 <lauedmondyc@gmail.com>
2021-05-03 13:59:25 +0800

    test

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ function preload() {
 }
 
 function moveleft() {
+  // context. clearRect(0, 0, game_size[0], game_size[1];
   playerx -= 5
 }
 
@@ -23,33 +24,31 @@ function jump() {
 
 function setup() {
   createCanvas(game_size[0], game_size[1]);
+
+  BACKGROUND.resize(game_size[0], game_size[1]);
+  frameRate(60);
+
+  document.addEventListener("keypress", function(event) {
+    if (event.key === 'q') {
+      moveleft()
+      alert(playerx)
+    }
+  });
+}
+
+function draw() {
   
   image(BACKGROUND, 0,0);
+  image(PLAYER, playerx, playery)
 
   for (repeat=0;repeat<20;repeat++)
   {
     image(TILE1, repeat*64, 496)
   }
 
-  BACKGROUND.resize(game_size[0], game_size[1]);
-  frameRate(60);
-
-  document.addEventListener("keydown", function(event) {
-    if (event.key === 'Enter') {
-      moveleft()
-      alert("working")
-    }
-  });
-}
-
-function draw() {
-  // context. clearRect(0, 0, game_size[0], game_size[1];
-  image(PLAYER, playerx, playery)
   textAlign(CENTER, CENTER);
   textSize(40);
   fill(255);
   // text('Game Title', game_size[0]/2, game_size[1]/3);        UNFINISHED
   // text('Game Subtitle', game_size[0]/2, game_size[1]*1/3+50);
-
-
 }
